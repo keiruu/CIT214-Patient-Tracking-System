021a70df4c860c1d1fc7943eb576096693a45a07
Abigail Kaye Unating <abbyunat@gmail.com>
2022-04-24 14:35:51 +0800

    Set layout for dashboard

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react'
 import Header from '../components/header'
-
+import Sidebar from '../components/sidebar'
+import styles from '../styles/Dashboard.module.css'
 export default class Dashboard extends Component {
   render() {
     return (
-      <div>
-         <Header/>
-        Dashboard
+      <div className={styles.dashboardContainer}>
+        <Sidebar/>
+        <div className={styles.leftContainer}>
+          <Header/>
+          <div>
+
+          </div>
+        </div>
       </div>
     )
   }
