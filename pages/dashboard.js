756ae5bece279af9cbf7e7a750075abd71b82dd6
Abigail Kaye Unating <abbyunat@gmail.com>
2022-04-24 12:36:17 +0800

    Added dashboard file and header component

---
+++ pages/dashboard.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import Header from '../components/header'
+
+export default class Dashboard extends Component {
+  render() {
+    return (
+      <div>
+         <Header/>
+        Dashboard
+      </div>
+    )
+  }
+}
