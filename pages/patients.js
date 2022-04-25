c7fbd59baa7755f6a6cccf8497f123ff901d423a
Abigail Kaye Unating <abbyunat@gmail.com>
2022-04-25 17:10:40 +0800

    Added patients page and css

---
+++ pages/patients.js
@@ -0,0 +1,20 @@
+import PatientTable from '../components/patientTable'
+import Header from '../components/header'
+import Sidebar from '../components/sidebar'
+import styles from '../styles/Patients.module.css'
+import React from 'react'
+ 
+export default function Patients() {
+
+  return (
+    <div className={styles.patientsContainer}>
+      <Sidebar/>
+    <div className={styles.leftContainer}>
+      <Header/>
+      <div className={styles.table}>
+        <PatientTable/>
+      </div>
+    </div>
+  </div>
+  )
+ }
