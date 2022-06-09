05177edfa092a87d4c24a6ecf8c3f72da037fe4a
Abigail Kaye Unating <abbyunat@gmail.com>
2022-06-10 01:13:25 +0800

    Added prep code for profile

--- src/authContext.js
+++ src/authContext.js
@@ -1,5 +1,5 @@
 import React, { createContext, useContext, useEffect, useState } from 'react'
-import { onAuthStateChanged, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut, sendPasswordResetEmail, updateProfile } from 'firebase/auth'
+import { onAuthStateChanged, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut, sendPasswordResetEmail, updateProfile, updateEmail, updatePassword } from 'firebase/auth'
 import { auth } from '../src/firebase'
 import styles from '../styles/Home.module.css'
 
@@ -71,9 +71,23 @@ export const AuthContextProvider = ({children}) => {
     await signOut(auth)
     setLoading(false)
   }
+
+  const updateDisplayName = (name) => {
+    return updateProfile(currentUser, {
+      displayName: name,
+    })
+  }
+
+  const updateUserEmail = (email) => {
+    return updateEmail(currentUser, email)
+  }
+
+  const updateUserPass = (password) => {
+    return updatePassword(currentUser, password)
+  }
   
   return (
-    <AuthContext.Provider value={{ currentUser, login, signup, logout, userUID, resetPassword, userName }}>
+    <AuthContext.Provider value={{ updateDisplayName, updateUserEmail, updateUserPass, currentUser, login, signup, logout, userUID, resetPassword, userName }}>
         {loading ? 
             <div className={styles.loader}>
                 <BounceLoader color="#6C71F8" loading={loading} size={105} css={ 
